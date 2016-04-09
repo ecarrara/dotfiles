6ffd02b56be5f63ba4db9380e2e88d684f203a41
Erle Carrara <carrara.erle@gmail.com>
2016-04-09 11:41:51 -0300

    oh my zsh :)

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOTFILES_DIR=$(dirname $(readlink -f "$0"))
+
+if [ ! -n "$ZSH" ]; then
+  ZSH=~/.oh-my-zsh
+fi
+
+if [ ! -d "$ZSH" ]; then
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
+fi
+
+ln -f -s $DOTFILES_DIR/zshrc ~/.zshrc
---
+++ zshrc
@@ -0,0 +1,9 @@
+ZSH=$HOME/.oh-my-zsh
+
+ZSH_THEME="afowler"
+
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
+export EDITOR=vim
